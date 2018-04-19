c226e7fd443fa8d1d61163209f2e7add940c3002
xugang <xugang@youzan.com>
2018-04-19 14:59:51 +0800

    注释

--- easy-cache-core/src/main/java/com/pop/easycache/interceptor/CacheInterceptor.java
+++ easy-cache-core/src/main/java/com/pop/easycache/interceptor/CacheInterceptor.java
@@ -2,20 +2,21 @@ package com.pop.easycache.interceptor;
 
 
 import com.pop.easycache.cache.Cache;
-import com.pop.easycache.interceptor.handler.NeedCacheHandler;
 import com.pop.easycache.interceptor.handler.CacheFlushHandler;
 import com.pop.easycache.interceptor.handler.CacheHandler;
+import com.pop.easycache.interceptor.handler.NeedCacheHandler;
 import com.pop.easycache.serialize.Serialize;
 import net.sf.cglib.proxy.MethodInterceptor;
 import net.sf.cglib.proxy.MethodProxy;
 
 import java.lang.reflect.Method;
+import java.util.concurrent.locks.Condition;
 
 /**
  * Created by xugang on 17/6/21.
  */
 public class CacheInterceptor implements MethodInterceptor {
-    private Cache cache;
+    private  Cache cache;
 
     private Serialize serialize;
 
